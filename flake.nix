7013de94cdd7611d85719442863bd7f2dfe0d030
Evelyn Park <evelyn@partnership.place>
2026-03-05 15:09:57 -0500

    unstable

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "A simple NixOS flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/unstable";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
